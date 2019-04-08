82b00e151a38746cec90836402cbaa76fb830d53
c001zero <nameziz@gmail.com>
2019-04-08 17:15:53 +0300

    Change object to array of objects

--- script.js
+++ script.js
@@ -3,17 +3,20 @@ let money,
     time;
 money = prompt("Ваш бюджет на месяц?", '');
 time = prompt("Введите дату в формате YYYY-MM-DD", '');
-let expenses = {},
+let expenses = [],
     optionalExpenses = {};
 for (let i = 0; i < 2; i++){
-    expenses.answer1 = prompt("Введите обязательную статью расходов в этом месяце", '');
-    expenses.answer2 = prompt("Во сколько обойдется?", '');
+    expenses.push({'answer1': prompt("Введите обязательную статью расходов в этом месяце", ''),
+                'answer2': prompt("Во сколько обойдется?", '')});
 }
 let appData = {
     'money': money,
     'timeData': time,
     'expenses': expenses,
-    'optionalExpenses': optionalExpenses
+    'optionalExpenses': optionalExpenses,
+    'income': [],
+    'savings': false
 };
-
-console.log(appData.money + "\n" + appData.timeData);
+console.log(appData);
+let result = (money - expenses[0].answer2 - expenses[1].answer2) / 30;
+alert("Ваш бюджет на день: " + result);
